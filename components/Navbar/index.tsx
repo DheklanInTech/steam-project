4caf50dc89424c58874583d909781cd7607c016c
declan <declanintech@gmail.com>
2022-12-07 10:51:59 +0100

    commit

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { BiMenuAltRight } from "react-icons/bi";
+import { ethers } from "ethers";
 import {
   Nav,
   Hamburger,
@@ -16,11 +17,81 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [learn, setLearn] = useState(false);
-  const [connectWallet, setConnectWallet] = useState(false);
+  // const [connectWallet, setConnectWallet] = useState(false);
   const [mobilenav, setMobilenav] = useState(false);
-  const handleConnectWallet = () => {
-    setConnectWallet(!connectWallet);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [currentAccount, setCurrentAccount] = useState("wallet");
+  const [currentAccountCol, setCurrentAccountCol] = useState<string>("Wallet");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  /**
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      setCurrentAccountCol(splitter(accounts[0]));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
+
+const splitter = (value:string):string =>{
+   const col = value.slice(0,5);   
+  const col2 = value.slice(38,42);
+
+  return (col+"..."+col2);
+}
+
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [currentAccountCol,currentAccount]);
+
+  // const handleConnectWallet = () => {
+  //   setConnectWallet(!connectWallet);
+  // };
 
   const handleMobileNav = () => {
     setMobilenav(!mobilenav);
@@ -92,8 +163,8 @@ const Navbar = () => {
             <Link href="/stake">
               <MenuLinks onClick={() => setIsOpen(!isOpen)}>Stake</MenuLinks>
             </Link>
-            <Button type="button" onClick={handleConnectWallet}>
-              {!connectWallet ? <span>Wallet</span> : <span>Disconnect</span>}{" "}
+            <Button type="button" onClick={connectWallet}>
+              {!currentAccount ? (<span>Wallet</span>) : (<span>{currentAccountCol}</span>)}{""}
             </Button>
           </Menu>
         </Container>
